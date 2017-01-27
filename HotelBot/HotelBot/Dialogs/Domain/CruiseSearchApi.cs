b573db4afc9c347481fb198b5e2e018bec1e3f33
d V <d V>
2017-01-27 14:46:54 -0500

    Live parametrized cruise search

--- HotelBot/HotelBot/Dialogs/Domain/CruiseSearchApi.cs
+++ HotelBot/HotelBot/Dialogs/Domain/CruiseSearchApi.cs
@@ -6,17 +6,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using Newtonsoft.Json;
+using HotelBot.Models;
 
 namespace HotelBot.Domain
 {
     public class CruiseSearchApi
     {
-        public static string Search()
+        private static List<string> ports = new List<string> { "LAX", "MIA", "FLL", "TPA" };
+        private static List<string> destinations = new List<string> { "A", "C", "BH", "E" };
+        private static List<string> months = new List<string> { "01", "02", "03", "04", "05","06","07","08","09","10","11","12" };
+        private static List<string> years = new List<string> { "2016", "2017", "2018", "2019" };
+
+        public static string Search(CruiseSearch state)
         {
+            var destination = destinations[(int)state.Destination];
+            var port = ports[(int)state.Port];
+            var numAdults = (int)state.Guests;
+            var date = months[(int)state.Month] + years[(int)state.Year];
 
             string url =
                 String.Format(
-                    "http://www.carnival.com/bookingengine/api/search?exclDetails=false&layout=grid&numAdults=2&numChildren=0&pageNumber=1&pageSize=8&showBest=true&sort=FromPrice&useSuggestions=true");
+                    "http://www.carnival.com/bookingengine/api/search?exclDetails=false&layout=grid&numAdults={0}&datFrom={1}&datTo={1}&dest={2}&port={3}&numChildren=0&pageNumber=1&pageSize=8&showBest=true&sort=FromPrice&useSuggestions=true",
+                    numAdults, date, destination, port);
             using (WebClient client = new WebClient())
             {
                 string json = client.DownloadString(url);
@@ -24,7 +35,7 @@ namespace HotelBot.Domain
                 JavaScriptSerializer json_serializer = new JavaScriptSerializer();
                 var minPrice = results.Results.Itineraries.Select(a => a.LeadSailing).Min(a => a.FromPrice);
                 var itin = results.Results.Itineraries.Where(a => a.LeadSailing.FromPrice == minPrice).First();
-                var msg = String.Format("We found a cruise with cheapest rate of {0} on {1}", itin.LeadSailing.FromPrice, itin.LeadSailing.DepartureDate);
+                var msg = String.Format("We found a cruise with cheapest rate of {0:C} on {1}", itin.LeadSailing.FromPrice, String.Format("{0:dd MMM yyyy}", itin.LeadSailing.DepartureDate));
                 return msg;
             }
 
