871a7ed21a27bc5693a1d801fe2d75257ab34e1f
d V <d V>
2017-01-27 15:23:02 -0500

    Room Service

--- HotelBot/HotelBot/Models/RoomService.cs
+++ HotelBot/HotelBot/Models/RoomService.cs
@@ -6,10 +6,49 @@ using System.Web;
 
 namespace HotelBot.Models
 {
+    public enum Beverages
+    {
+        Nothing,
+        Tea,
+        Coffee,
+        OrangeJuice,
+        AppleJuice
+    }
+
+    public enum Salads
+    {
+        VeganSalad,
+        GardenSalad,
+        CaesarSalad,
+        GreekSalad
+    }
+
+    public enum BreakfastBites
+    {
+        SesameBagel,
+        PlainBagelWithCreamCheese,
+        Croissant,
+        CheeriosWithMilk,
+        Oatmeal
+    }
+
+    public enum Beers
+    {
+        Budlight,
+        SamuelAdams,
+        Corona,
+        Budweiser,
+        Heikenen
+    }
+
+
     [Serializable]
     public class RoomService
     {
-        public string FoodRequest;
+        public List<BreakfastBites> BreakfastBites;
+        public Salads Salads;
+        public List<BreakfastBites> Beverages;
+        public Beers Beer;
 
         public static IForm<RoomService> BuildForm()
         {
